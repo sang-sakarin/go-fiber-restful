c980f8008f84ee617dfd721c0ad57de691b1639f
Sang sakarin <sang_sakarin@outlook.com>
2021-10-22 15:23:53 +0700

    fix: import pkg cycle #2

--- pkg/databases.go
+++ pkg/databases.go
@@ -2,26 +2,21 @@ package pkg
 
 import (
 	"fmt"
+	"go-fiber-restful/pkg/databases"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
 	"os"
 )
 
-var DB *gorm.DB
-
-func UseDatabase(cfg Config) {
-
+func (extendApp *ExtendApp) UseDatabase(cfg Config) {
 	var dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s", cfg.DatabaseHost, cfg.DatabaseUser, cfg.DatabasePassword, cfg.DatabaseName)
 	var err error
 
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	databases.DBConn, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal("Failed to connect to database. \n", err)
 		os.Exit(2)
 	}
-
-	//DB.Migrator().AutoMigrate(&users.User{})
-	//DB.Migrator().AutoMigrate(&news.News{})
 }
